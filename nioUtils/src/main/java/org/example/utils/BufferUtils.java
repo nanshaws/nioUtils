41d4ee778dd3a0f8a1afbd3f7b55eb538d8e62c9
cover <98329722+nanshaws@users.noreply.github.com>
2023-12-01 12:40:52 +0800

    完成了byteBuffer对任何数据类型的put和get的封装
    完成了byteBuffer对任何数据类型的put和get的封装

--- nioUtils/src/main/java/org/example/utils/BufferUtils.java
+++ nioUtils/src/main/java/org/example/utils/BufferUtils.java
@@ -13,10 +13,16 @@ public class BufferUtils {
     FloatBuffer floatBuffer;
     LongBuffer longBuffer;
     ShortBuffer shortBuffer;
+    //get的时候的位置
+    Integer position=0;
+    //真正的从0开始的索引
+
+    int readable=1;
+    int Tindex=0;
     public static Integer[] array=new Integer[1000];
     public volatile static int index=0;
     public BufferUtils(Integer capacity,Class obj) {
-        String objName = obj.getName();
+        String objName = obj.getSimpleName();
 
         switch (objName) {
             case "int","Integer"->intBuffer=IntBuffer.allocate(capacity);
@@ -45,17 +51,18 @@ public class BufferUtils {
 
     public void put(Integer integer){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(integer));
-        } else if (intBuffer!=null) {
+            byteBuffer.putInt(integer);
+        }else if (intBuffer!=null) {
             intBuffer.put(integer);
-        } else{
-            new RuntimeException("除开int和byte，其他Buffer不支持存放int");
+        }else {
+            new RuntimeException("其他Buffer不支持存放Integer对象");
         }
+
     }
 
     public void put(Float arg){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(arg));
+            byteBuffer.putFloat(arg);
         } else if (floatBuffer!=null) {
             floatBuffer.put(arg);
         } else{
@@ -65,7 +72,7 @@ public class BufferUtils {
 
     public void put(Character character){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(character));
+            byteBuffer.putChar(character);
         } else if (character!=null) {
             charBuffer.put(character);
         } else{
@@ -75,7 +82,7 @@ public class BufferUtils {
 
     public void put(Double double1){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(double1));
+            byteBuffer.putDouble(double1);
         } else if (doubleBuffer!=null) {
             doubleBuffer.put(double1);
         } else{
@@ -85,7 +92,7 @@ public class BufferUtils {
 
     public void put(Long long1){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(long1));
+            byteBuffer.putLong(long1);
         } else if (longBuffer!=null) {
             longBuffer.put(long1);
         } else{
@@ -95,11 +102,19 @@ public class BufferUtils {
 
     public void put(Short short1){
         if (byteBuffer!=null){
-            byteBuffer.put(toByteArray(short1));
+            byteBuffer.putShort(short1);
         } else if (shortBuffer!=null) {
             shortBuffer.put(short1);
         } else{
             new RuntimeException("除开short和byte，其他Buffer不支持存放short");
+        }
+    }
+
+    public void put(Boolean bool){
+        if (byteBuffer!=null){
+            byteBuffer.put(bool?(byte)1:(byte)0);
+        } else{
+            new RuntimeException("除开byte，其他Buffer不支持存放bool");
         }
     }
 
@@ -112,19 +127,89 @@ public class BufferUtils {
     }
 
     public Object get(Class aclazz){
+
         Object obj=null;
-        if (byteBuffer!=null){
-            index--;
-            int length=array[index];
-            byte[] bytes=new byte[array[index]];
-            for (int i=0;i<length;i++){
-                byte b = byteBuffer.get(i);
-                bytes[i]=b;
+        String objName = aclazz.getSimpleName();
+
+        switch (objName) {
+            case "int","Integer"->{
+                int anInt = byteBuffer.getInt(position);
+                addFourByte();
+                return anInt;
             }
-            obj = JSON.parseObject(bytes, aclazz);
+            case "char","Character"->{
+                char aChar = byteBuffer.getChar(position);
+                addTwoByte();
+                return aChar;
+            }
+            case "byte","Byte"->{
+                byte aByte = byteBuffer.get(position);
+                addOneByte();
+                return aByte;
+            }
+            case "double","Double"->{
+                double aDouble = byteBuffer.getDouble(position);
+                addEightByte();
+                return aDouble;
+            }
+            case "float","Float"->{
+                Float anFloat = byteBuffer.getFloat(position);
+                addFourByte();
+                return anFloat;
+            }
+            case "long","Long"->{
+                Long aLong = byteBuffer.getLong(position);
+                addEightByte();
+                return aLong;
+            }
+            case "short","Short"->{
+                short aShort = byteBuffer.getShort(position);
+                addTwoByte();
+                return aShort;
+            }
+            case "boolean","Boolean"->{
+                byte b = byteBuffer.get(position);
+                Boolean aBool = (b == 0x00) ? false : true;
+                addOneByte();
+                return aBool;
+            }
+            default -> new RuntimeException("输入的参数无法识别");
+        }
+
+        if (byteBuffer!=null){
+            byte[] bytes=new byte[array[Tindex]];
+            for (;position<array[Tindex];position++){
+                byte b = byteBuffer.get(position);
+                bytes[position]=b;
+            }
+            if (array[Tindex]>10){
+                obj = JSON.parseObject(bytes, aclazz);
+            }
+            if (Tindex<index-1){
+                Tindex++;
+                array[Tindex]=position+array[Tindex];
+            }
+
         }
         return obj;
     }
 
+    public void addFourByte(){
+        position=position+4;
+        array[Tindex]=array[Tindex]+4;
+    }
+    public void addTwoByte(){
+        position=position+2;
+        array[Tindex]=array[Tindex]+2;
+    }
 
+    public void addOneByte(){
+        position=position+1;
+        array[Tindex]=array[Tindex]+1;
+    }
+
+    public void addEightByte(){
+        position=position+8;
+        array[Tindex]=array[Tindex]+8;
+    }
 }
--- nioUtils/src/test/java/BufferUtilsTest.java
+++ nioUtils/src/test/java/BufferUtilsTest.java
@@ -7,7 +7,7 @@ public class BufferUtilsTest {
     @Test
     public  void  test001() {
         BufferUtils bufferUtils=new BufferUtils(60);
-        Main main=new Main("nh","123456");
+        Main main=new Main("nhl","123456");
         bufferUtils.put(main);
         Main o = (Main) bufferUtils.get(Main.class);
         System.out.println(o);
@@ -15,12 +15,58 @@ public class BufferUtilsTest {
 
     @Test
     public  void  test002() {
-        BufferUtils bufferUtils=new BufferUtils(60);
+        BufferUtils bufferUtils=new BufferUtils(1000);
+        Main main=new Main("nh","123456");
+        Main main1=new Main("cyl","123456");
+        Main main2=new Main("cylsdfas","123456");
+        bufferUtils.put(main);
+        bufferUtils.put(main1);
+        bufferUtils.put(main2);
+        Main o = (Main) bufferUtils.get(Main.class);
+        Main o1 = (Main) bufferUtils.get(Main.class);
+        System.out.println(o);
+        System.out.println(o1);
+
+    }
+
+    @Test
+    public void test003(){
+        BufferUtils bufferUtils=new BufferUtils(1000);
         Main main=new Main("nh","123456");
         Main main1=new Main("cyl","123456");
         bufferUtils.put(main);
+        bufferUtils.put(1);
         bufferUtils.put(main1);
         Main o = (Main) bufferUtils.get(Main.class);
         System.out.println(o);
+        Object o1 = bufferUtils.get(Integer.class);
+        System.out.println(o1);
+        Object o2 = bufferUtils.get(Main.class);
+        System.out.println(o2);
+        bufferUtils.put(3.14f);
+        bufferUtils.put(3.400);
+        bufferUtils.put(989l);
+        bufferUtils.put(true);
+
+        Object o3 = bufferUtils.get(Float.class);
+        Object o4 = bufferUtils.get(Double.class);
+        Object o5 = bufferUtils.get(Long.class);
+        Object o6 = bufferUtils.get(Boolean.class);
+        System.out.println(o3);
+        System.out.println(o4);
+        System.out.println(o5);
+        System.out.println(o6);
+        Main main14=new Main("cyl","123456");
+        bufferUtils.put(main14);
+        System.out.println(bufferUtils.get(Main.class));
+    }
+
+    @Test
+    public void test004(){
+        BufferUtils bufferUtils=new BufferUtils(1000);
+        bufferUtils.put(1);
+        bufferUtils.put(3);
+        System.out.println(bufferUtils.get(Integer.class));
+        System.out.println(bufferUtils.get(Integer.class));
     }
 }
